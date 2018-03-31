889fb70643808967998ba77bf5785737f2db9441
Drew Immerman <30954849+DrewImm@users.noreply.github.com>
2018-03-31 12:59:02 -0400

    Created config

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 // Load cli
 let cli = require("./lib/cli")();
+let config = new (require("./lib/config"))();
+
+// Check if we need to create a config
+if (cli.createConfig) {
+    config.create();
+    process.exit();
+}
+
+// Get the existing config
+try {
+    config = config.get();
+}
+catch (ex) {
+    console.log("Could not load config.");
+    process.exit();
+}
