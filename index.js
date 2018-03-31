4f76cca77e3c8bdbb3369dada853b737c9594d06
Drew Immerman <30954849+DrewImm@users.noreply.github.com>
2018-03-31 13:15:54 -0400

    Start npm server

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const shell = require("child_process");
+
 // Load cli
 let cli = require("./lib/cli")();
 let config = new (require("./lib/config"))();
@@ -15,4 +17,16 @@ try {
 catch (ex) {
     console.log("Could not load config.");
     process.exit();
+}
+
+// Start the engine
+
+/// Check for input server command
+if (config.input && config.input.directory && config.input.runcmd) {
+
+    // Run the command
+    console.log("Starting server...");
+    let cmd = "cd " + config.input.directory + " && " + config.input.runcmd;
+    shell.execSync(cmd);
+
 }
